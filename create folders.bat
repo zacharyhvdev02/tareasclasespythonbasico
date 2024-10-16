03f85281e792e161e323569b6842ea001d314a2a
zacharyhvdev02 <117555232+zacharyhvdev02@users.noreply.github.com>
2024-10-15 19:20:37 -0600

    first commit

---
+++ create folders.bat
@@ -0,0 +1,19 @@
+mkdir "Proyectos\Calculadora"
+mkdir "Proyectos\Lista de compras"
+mkdir "Proyectos\Lista de tareas por hacer"
+mkdir "Proyectos\Juego gato"
+
+mkdir "Clase 1\Tareas\Presentacion"
+mkdir "Clase 2\Tareas\Float"
+mkdir "Clase 2\Tareas\Double"
+mkdir "Clase 2\Tareas\Complex"
+mkdir "Clase 3\Tareas\Calculadora"
+mkdir "Clase 5\Tareas\Lista de tareas por hacer"
+mkdir "Clase 6\Tareas\Lista de compras"
+mkdir "Clase 7\Tareas\File handling"
+mkdir "Clase 8\Tareas\Error handling"
+mkdir "Clase 9\Tareas\Libraries built-in"
+mkdir "Clase 10\Tareas\UI"
+
+echo Folders created successfully!
+pause
