ee2a68c43a61dbece9eeeb9fa928042eb37895e4
zacharyhvdev02 <117555232+zacharyhvdev02@users.noreply.github.com>
2024-10-15 19:39:32 -0600

    python header

---
+++ Clase 2/Tareas/Float/main.py
@@ -0,0 +1,6 @@
+''' Bienvenido!
+Vamos a aprender un poco de los flotantes en Python.
+Instrucciones:
+EL código a continuación no está funcionando, necesitamos arreglarlo con el fin de que imprima el resultado correcto.
+Sigue las indicaciones y logra que el código funcione correctamente!
+'''
