dce654f27da9045b525b5f1ea69dbac82a818ee8
zacharyhvdev02 <117555232+zacharyhvdev02@users.noreply.github.com>
2024-10-15 19:23:38 -0600

    added files

---
+++ create files.bat
@@ -0,0 +1,18 @@
+type nul > "Proyectos\Calculadora\main.py"
+type nul > "Proyectos\Lista de compras\main.py"
+type nul > "Proyectos\Lista de tareas por hacer\main.py"
+type nul > "Proyectos\Juego gato\main.py"
+
+type nul > "Clase 1\Tareas\Presentacion\main.py"
+type nul > "Clase 2\Tareas\Float\main.py"
+type nul > "Clase 2\Tareas\Double\main.py"
+type nul > "Clase 2\Tareas\Complex\main.py"
+type nul > "Clase 3\Tareas\Calculadora\main.py"
+type nul > "Clase 5\Tareas\Lista de tareas por hacer\main.py"
+type nul > "Clase 6\Tareas\Lista de compras\main.py"
+type nul > "Clase 7\Tareas\File handling\main.py"
+type nul > "Clase 8\Tareas\Error handling\main.py"
+type nul > "Clase 9\Tareas\Libraries built-in\main.py"
+type nul > "Clase 10\Tareas\UI\main.py"
+
+echo Folders and Python files created successfully!
